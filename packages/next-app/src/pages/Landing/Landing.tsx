d83ccec0d5120d76883d16e0b8791b805d3f30bb
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2023-03-20 01:22:24 +0530

    Landing page style changes

--- packages/next-app/src/pages/Landing/Landing.tsx
+++ packages/next-app/src/pages/Landing/Landing.tsx
@@ -13,13 +13,22 @@ export default function Landing() {
             className={inter.className}
             style={{
               margin: "10px",
-              fontSize: "3rem",
+              fontSize: "4rem",
               userSelect: "none",
             }}
           >
             deMark
           </h1>
-          <p className={inter.className}>Proof of Source for your Images</p>
+          <p
+            className={inter.className}
+            style={{
+              marginTop: "10px",
+              fontSize: "1.25rem",
+              userSelect: "none",
+            }}
+          >
+            Proof of Source for your Images
+          </p>
         </div>
         <ConnectButton showBalance={false} />
       </div>
