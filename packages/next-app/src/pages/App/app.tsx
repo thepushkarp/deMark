40c17a57647ecd5195fee35d88748cfb465ba286
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2023-03-21 02:22:28 +0530

    Add file size and some misc changes

--- packages/next-app/src/pages/App/app.tsx
+++ packages/next-app/src/pages/App/app.tsx
@@ -1,7 +1,8 @@
 import styles from "@/styles/Home.module.css";
 import { useStorageUpload } from "@thirdweb-dev/react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "@/components/Header/Header";
+import Image from "next/image";
 
 export default function App() {
   const [file, setFile] = useState<File | null>(null);
@@ -11,18 +12,30 @@ export default function App() {
     if (file) {
       const uploadUrl = await upload({
         data: [file],
-        options: { uploadWithGatewayUrl: true, uploadWithoutDirectory: true },
+        options: {
+          uploadWithGatewayUrl: false,
+          uploadWithoutDirectory: true,
+        },
       });
       alert(uploadUrl);
       setFile(null);
     }
   };
 
+  const isFileTooLarge = (file: File) => {
+    // Can't upload a file with size > 15MB
+    return file.size > 15 * 1024 * 1024;
+  };
+
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(null);
     if (e.target.files && e.target.files.length > 0) {
-      setFile(e.target.files[0]);
-    } else {
-      setFile(null);
+      const file = e.target.files[0];
+      if (isFileTooLarge(file)) {
+        alert("File size is too large");
+        return;
+      }
+      setFile(file);
     }
   };
 
@@ -35,6 +48,7 @@ export default function App() {
           <button onClick={uploadToIpfs} disabled={!file}>
             Upload
           </button>
+          <div>{file && <Image src={URL.createObjectURL(file)} alt="Preview" width={150} height={150} />}</div>
         </div>
       </main>
     </>
